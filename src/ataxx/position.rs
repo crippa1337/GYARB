660c88c2f774d5cc502d1a4e6f6a1bb909a042dd
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2023-03-04 14:14:58 +0100

    Display for Position and tests

--- src/ataxx/position.rs
+++ src/ataxx/position.rs
@@ -1,4 +1,14 @@
+use std::fmt::Display;
+
 use super::bitboard::BitBoard;
+
+#[derive(Debug, PartialEq)]
+pub enum FenError {
+    Illegal,
+    Turn,
+    HalfMoves,
+    FullMoves,
+}
 
 #[derive(Debug, PartialEq)]
 pub enum Side {
@@ -17,11 +27,15 @@ pub struct Position {
 }
 
 impl Position {
-    pub fn from_fen(fen: &str) -> Position {
+    pub fn from_fen(fen: &str) -> Result<Position, FenError> {
         let mut black = BitBoard(0);
         let mut white = BitBoard(0);
         let mut gaps = BitBoard(0);
         let fen: Vec<&str> = fen.split(' ').collect();
+
+        if fen.len() != 4 {
+            return Err(FenError::Illegal);
+        }
 
         let mut i = 0;
         for c in fen[0].chars() {
@@ -34,9 +48,6 @@ impl Position {
                     white |= BitBoard::from_index(i as u8);
                     i += 1;
                 }
-                '/' => {
-                    assert!(i % 7 == 0)
-                }
                 '-' => {
                     gaps |= BitBoard::from_index(i as u8);
                     i += 1;
@@ -44,7 +55,10 @@ impl Position {
                 z if z.is_ascii_digit() => {
                     i += z.to_digit(10).unwrap() as usize;
                 }
-                _ => panic!("{c} does not belong in this FEN String *facepalm*"),
+                '/' => {
+                    assert!(i % 7 == 0)
+                }
+                _ => return Err(FenError::Illegal),
             }
         }
 
@@ -54,21 +68,25 @@ impl Position {
         let turn = match fen[1] {
             "x" => Side::Black,
             "o" => Side::White,
-            _ => panic!(
-                "FEN String is invalid! Expected 'x' or 'o' for turn, got {}",
-                fen[1]
-            ),
+            _ => return Err(FenError::Turn),
         };
 
-        let half_moves: u8 = fen[2].parse().unwrap();
-        assert!(
-            half_moves <= 100,
-            "A half-move clock of {half_moves} is illegal"
-        );
+        let half_moves = match fen[2].parse::<u8>() {
+            Ok(half_moves) => {
+                if half_moves > 100 {
+                    return Err(FenError::HalfMoves);
+                }
+                half_moves
+            }
+            Err(_) => return Err(FenError::HalfMoves),
+        };
 
-        let full_moves: u8 = fen[3].parse().unwrap();
+        let full_moves = match fen[3].parse::<u8>() {
+            Ok(full_moves) => full_moves,
+            Err(_) => return Err(FenError::FullMoves),
+        };
 
-        Position {
+        Ok(Position {
             occupied,
             black,
             white,
@@ -76,7 +94,7 @@ impl Position {
             turn,
             half_moves,
             full_moves,
-        }
+        })
     }
 
     pub fn get_fen(&self) -> String {
@@ -108,14 +126,18 @@ impl Position {
                 empty += 1;
             }
 
-            // Slash at the end of a row, but not in the beginning or end
-            if i % 7 == 6 && i != 0 && i != 48 {
+            // Slash at the end of a row, but not at the end
+            if i % 7 == 6 && i != 48 {
                 if empty > 0 {
                     fen.push_str(&empty.to_string());
                     empty = 0;
                 }
                 fen.push('/');
             }
+        }
+
+        if empty > 0 {
+            fen.push_str(&empty.to_string());
         }
 
         fen.push(' ');
@@ -134,14 +156,38 @@ impl Position {
     }
 }
 
+impl Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..49u8 {
+            let idx = BitBoard::from_index(i);
+
+            if self.black & idx != BitBoard(0) {
+                write!(f, "x")?;
+            } else if self.white & idx != BitBoard(0) {
+                write!(f, "o")?;
+            } else if self.gaps & idx != BitBoard(0) {
+                write!(f, "#")?;
+            } else {
+                write!(f, "-")?;
+            }
+
+            if i % 7 == 6 {
+                writeln!(f)?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn fens() {
+    fn success_fen() {
         let fen = "x5o/7/7/7/7/7/o5x x 0 1";
-        let p = Position::from_fen(fen);
+        let p = Position::from_fen(fen).unwrap();
         assert_eq!(p.turn, Side::Black);
         assert_eq!(p.occupied, BitBoard(0x1040000000041));
         assert_eq!(p.black, BitBoard(0x1000000000001));
@@ -150,5 +196,43 @@ mod tests {
         assert_eq!(p.half_moves, 0);
         assert_eq!(p.full_moves, 1);
         assert_eq!(p.get_fen(), fen);
+    }
+
+    #[test]
+    fn get_fen() {
+        let fens = [
+            "x5o/7/7/7/7/7/o5x x 0 1",
+            "x5o/7/2-1-2/7/2-1-2/7/o5x x 0 1",
+            "x5o/7/3-3/2-1-2/3-3/7/o5x x 0 1",
+            "7/7/7/7/7/7/7 x 0 1",
+            "7/7/7/7/7/7/7 o 0 1",
+            "7/7/7/7/7/7/7 x 100 1",
+            "7/7/7/7/7/7/7 o 100 1",
+            "7/7/7/7/7/7/7 x 0 100",
+            "7/7/7/7/7/7/7 o 0 100",
+            "7/7/7/7/7/7/7 x 100 200",
+            "7/7/7/7/7/7/7 o 100 200",
+        ];
+
+        for fen in fens.iter() {
+            let p = Position::from_fen(fen).unwrap();
+            assert_eq!(p.get_fen(), *fen);
+        }
+    }
+
+    #[test]
+    fn fen_error() {
+        let fens = [
+            "x5o/7/7/7/7/7/o5x",
+            "x5o/7/7/7/7/7/o5x x",
+            "x5o/7/7/7/7/7/o5x x 0",
+            "x5o/7/2-1-2/7/2-1-2/7/o5x",
+            "x5o/7/2-1-2/7/2-1-2/7/o5x x",
+            "x5o/7/2-1-2/7/2-1-2/7/o5x x 0",
+        ];
+
+        for fen in fens.iter() {
+            assert!(Position::from_fen(fen).is_err());
+        }
     }
 }
