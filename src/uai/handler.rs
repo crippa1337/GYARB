0c5932f3eff8a4eda2be4f2eccf711ea9f2fd439
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2023-03-10 22:25:58 +0100

    coolness

--- src/uai/handler.rs
+++ src/uai/handler.rs
@@ -1,3 +1,5 @@
+use crate::ataxx::position::Position;
+
 use super::state::State;
 
 pub fn main() {
@@ -6,7 +8,42 @@ pub fn main() {
     loop {
         let mut input = String::new();
         std::io::stdin().read_line(&mut input).unwrap();
+        let token: Vec<&str> = input.split_whitespace().collect();
 
-        let mut tokens = input.split_whitespace();
+        match token[0] {
+            "uai" => {
+                println!("id name Kurt");
+                println!("id author Crippa");
+                println!("uaiok");
+            }
+
+            "isready" => {
+                println!("readyok");
+            }
+
+            "position" => {
+                let mut fen = String::new();
+
+                if token[1] == "fen" {
+                    for f in token.iter().take(6).skip(2) {
+                        fen.push(' ');
+                        fen.push_str(f);
+                    }
+                } else if token[1] == "startpos" {
+                    fen = "x5o/7/7/7/7/7/o5x x 0 1".to_string();
+                } else {
+                    continue;
+                }
+
+                println!("FEN: {}", fen);
+
+                match Position::from_fen(&fen) {
+                    Ok(_) => state.pos = Position::from_fen(&fen).unwrap(),
+                    Err(_) => continue,
+                }
+            }
+
+            _ => {}
+        }
     }
 }
