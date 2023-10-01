c78fdbc191ff53640fd63884d199cacd2b0c0b2b
BlueKossa <dexter@stormdal.se>
2023-10-01 23:00:33 +0200

    Added basic moving

--- gui/src/main.py
+++ gui/src/main.py
@@ -14,6 +14,7 @@ class Game:
         self.player = Entity(name='X', color=color.orange)
         self.turn_text = t = Text(text=f'Turn: {self.player.name}',
                                   scale=2, position=(-0.85, 0.45), color=self.player.color)
+        self.selected = None
 
     def swap_turn(self):
         if self.player.name == 'X':
@@ -25,11 +26,36 @@ class Game:
 
         self.turn_text.text = f'Turn: {self.player.name}'
         self.turn_text.color = self.player.color
-
+        self.selected = None
 
 game = Game()
 player = game.player
 board = [[None for _ in range(7)] for _ in range(7)]
+selected = None
+
+def create_new(tile):
+    pos = tile.position
+    x = int(pos.x)
+    y = int(pos.y)
+    # Update surrounding tiles
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            x_rel = x + i
+            y_rel = y + j
+            if 6 < x_rel or x_rel < 0:
+                continue
+            if 6 < y_rel or y_rel < 0:
+                continue
+            t = board[x + i][y + j]
+            if t.text != None:
+                t.text = player.name
+                t.text_color = player.color
+
+def jump(old, new):
+    old.text = ''
+    old.text = None
+    old.text_color = None
+    create_new(new)
 
 # Initial board state
 for y in range(7):
@@ -38,11 +64,35 @@ for y in range(7):
         board[x][y] = b
 
         def on_click(b=b):
-            b.text = player.name
-            b.text_color = player.color
-            game.swap_turn()
+            selected = game.selected
+            # Make a move
+            if selected != None and b.text == None:
+                # Get the Chebyshev Distance
+                p1 = selected.position
+                p2 = b.position
+                distance = max(abs(p1.x - p2.x), abs(p1.y - p2.y))
+
+                if distance == 2.:
+                    jump(selected, b)
+                elif distance == 1.:
+                    create_new(b)
+                else:
+                    return
+                
+                # Set the new squares properties
+                b.text = player.name
+                b.text_color = player.color
+                
+                # Reset for next turn
+                game.swap_turn()
+            # Select a tile
+            elif b.text == player.name:
+                game.selected = b
+
 
         b.on_click = on_click
+        
+        
 
 board[6][0].text, board[6][0].text_color = 'X', color.orange
 board[0][6].text, board[0][6].text_color = 'X', color.orange
