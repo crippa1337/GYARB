9794d8fcbbd893edb198c4c99eca97b3046f7d64
BlueKossa <dexter@stormdal.se>
2023-10-02 18:37:55 +0200

    Refactoring

--- gui/src/main.py
+++ gui/src/main.py
@@ -1,4 +1,5 @@
 from ursina import *
+from fen import generate_fen, parse_fen
 
 if __name__ == '__main__':
     app = Ursina()
@@ -9,12 +10,64 @@ camera.position = (3, 3)
 Text.default_resolution *= 2
 
 
+
+
+
 class Game:
-    def __init__(self) -> None:
+    selected: Button | None
+    def __init__(self, board) -> None:
         self.player = Entity(name='X', color=color.orange)
         self.turn_text = t = Text(text=f'Turn: {self.player.name}',
                                   scale=2, position=(-0.85, 0.45), color=self.player.color)
+        self.board = board
         self.selected = None
+    
+    def move_dup(self, new):
+        pos = new.position
+        x = int(pos.x)
+        y = int(pos.y)
+        # Update surrounding tiles
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                x_rel = x + i
+                y_rel = y + j
+                if 6 < x_rel or x_rel < 0:
+                    continue
+                if 6 < y_rel or y_rel < 0:
+                    continue
+                t = self.board[x + i][y + j]
+                if t.text is not None:
+                    t.text = self.player.name
+                    t.text_color = self.player.color
+    
+    def move_jmp(self, new):
+        self.selected.text = ''
+        self.selected.text_color = None
+        self.move_dup(new)
+
+    def click_tile(self, tile):
+        if self.selected is not None and (tile.text is None or tile.text == ''):
+            # Get the Chebyshev Distance
+            p_1 = self.selected.position
+            p_2 = tile.position
+            chebyshev_distance = max(abs(p_1.x - p_2.x), abs(p_1.y - p_2.y))
+
+            if chebyshev_distance == 2.:
+                self.move_jmp(tile)
+            elif chebyshev_distance == 1.:
+                self.move_dup(tile)
+            else:
+                return
+            
+            # Set the new squares properties
+            tile.text = self.player.name
+            tile.text_color = self.player.color
+            
+            # Reset for next turn
+            self.swap_turn()
+        # Select a tile
+        elif tile.text == self.player.name:
+            self.selected = tile
 
     def swap_turn(self):
         if self.player.name == 'X':
@@ -28,76 +81,21 @@ class Game:
         self.turn_text.color = self.player.color
         self.selected = None
 
-game = Game()
-player = game.player
-board = [[None for _ in range(7)] for _ in range(7)]
-selected = None
-
-def create_new(tile):
-    pos = tile.position
-    x = int(pos.x)
-    y = int(pos.y)
-    # Update surrounding tiles
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            x_rel = x + i
-            y_rel = y + j
-            if 6 < x_rel or x_rel < 0:
-                continue
-            if 6 < y_rel or y_rel < 0:
-                continue
-            t = board[x + i][y + j]
-            if t.text != None:
-                t.text = player.name
-                t.text_color = player.color
-
-def jump(old, new):
-    old.text = ''
-    old.text = None
-    old.text_color = None
-    create_new(new)
-
-# Initial board state
-for y in range(7):
-    for x in range(7):
-        b = Button(parent=scene, position=(x, y), scale=1, radius=0)
-        board[x][y] = b
-
-        def on_click(b=b):
-            selected = game.selected
-            # Make a move
-            if selected != None and b.text == None:
-                # Get the Chebyshev Distance
-                p1 = selected.position
-                p2 = b.position
-                distance = max(abs(p1.x - p2.x), abs(p1.y - p2.y))
-
-                if distance == 2.:
-                    jump(selected, b)
-                elif distance == 1.:
-                    create_new(b)
-                else:
-                    return
-                
-                # Set the new squares properties
-                b.text = player.name
-                b.text_color = player.color
-                
-                # Reset for next turn
-                game.swap_turn()
-            # Select a tile
-            elif b.text == player.name:
-                game.selected = b
 
 
-        b.on_click = on_click
-        
-        
 
-board[6][0].text, board[6][0].text_color = 'X', color.orange
-board[0][6].text, board[0][6].text_color = 'X', color.orange
-board[0][0].text, board[0][0].text_color = 'O', color.azure
-board[6][6].text, board[6][6].text_color = 'O', color.azure
+
+
+
+
+def main():
+    game = Game([])
+    game.board = parse_fen("x6/-6/7/7/7/7/o5x")
+    print(generate_fen(game.board))
+    scene.game = game
+
+    
 
 if __name__ == '__main__':
+    main()
     app.run()
